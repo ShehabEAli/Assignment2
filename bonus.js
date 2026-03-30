fb1a24ccde4498697295fb759bc9c8ad380dcc92
ShehabEAli <shehabelsehelly@gmail.com>
2026-03-30 20:08:33 +0200

    For Explaining

--- bonus.js
+++ bonus.js
@@ -1,8 +1,3 @@
-/**
- * @param {number[]} arr
- * @param {number} k
- * @return {number}
- */
 /***********  first Solution which is  >> arr.includes() << takes O(n) **************/
 /***********  Time Complexity: O(n * k) **************/
 var findKthPositive = function(arr, k) {
@@ -19,13 +14,9 @@ var findKthPositive = function(arr, k) {
 };
 
 
-/***********  Another Fast Solution which is >> set.has() << takes O(1) **************/
+/***********  Second Solution which is (This is the fast solution)>> set.has() << takes O(1) *******/
 /***********  Time Complexity: O(n + k) **************/
-/**
- * @param {number[]} arr
- * @param {number} k
- * @return {number}
- */
+
 var findKthPositive = function(arr, k) {
     const set = new Set(arr);
     var missingCount = 1;
